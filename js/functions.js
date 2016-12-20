849042f0912cdf2e1903f4656539c4bac1e3e5e2
Gabe Campbell <gabe.campbell@icloud.com>
2016-12-19 20:32:55 -0600

    Completed 3.4.1 exercise

--- js/functions.js
+++ js/functions.js
@@ -6,14 +6,14 @@ var myNameIs = 'Gabe'; // TODO: Fill in your name here.
 // Create a function called 'sayHello' that takes a parameter 'name'.
 // When called, the function should log a message that says hello from the passed in name.
 function sayHello(name) {
-  console.log(name);
+  console.log("Hello " + name + "!");
 }
 // TODO: Call the function 'sayHello' passing the variable 'myNameIs' as a parameter.
 
 sayHello(myNameIs);
 // Don't modify the following line
 // It generates a random number between 1 and 100 and stores it in random
-// var random = Math.floor((Math.random()*100)+1);
+var random = Math.floor((Math.random()*100)+1);
 console.log(random);
 // TODO:
 // Create a function called 'isOdd' that takes a number as a parameter.
@@ -25,5 +25,5 @@ function isOdd(number) {
   console.log(oddOrEven);
 }
 
-// TODO: Call the function 'isOdd' passing the variable 'random' as a parameter.
 isOdd(random);
+// TODO: Call the function 'isOdd' passing the variable 'random' as a parameter.
