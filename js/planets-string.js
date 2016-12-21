25f37c5d03673982c747a3ad53e28a53e31b96cb
Gabe Campbell <gabe.campbell@icloud.com>
2016-12-21 12:40:36 -0600

    completed 3.5.3 bonus exercise

--- js/planets-string.js
+++ js/planets-string.js
@@ -18,6 +18,15 @@
     // Because it can be converted to html
 
     // Bonus: Create a second string that would display your planets in an undordered list.
+    function makeString() {
+      console.log("<ul>");
+      planetsArray.forEach(function(element, index, array){
+      console.log("    <li>" + element + "</li>");
+      });
+    console.log("</ul>");
+    };
+
+    makeString();
 
     //        You will need an opening AND closing <ul> tags around the entire string, and <li> tags around each planet.
     //        console.log() your results.
