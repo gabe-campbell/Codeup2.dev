8e59c9501e864ab4480905ad78a38ba37a015404
Gabe Campbell <gabe.campbell@icloud.com>
2017-02-20 15:10:15 -0600

    -Added learning outcomes to all Chapter 1 exercises -Modified filenames

--- js/add-content.js
+++ js/add-content.js
@@ -2,14 +2,14 @@
 // It then calls this date object and gets the current amount time and stores this in the variable hourNow
 // It then creates a variable called greeting to be used later in the script
 
-// The code then sets a conditional statement that checks what time it is and sets a different line of text based on that time and assigns it to greeting
-// If the time cannot be found, it will always display the default
-
-// The script finally calls on the document object to create HTML to display a header tag with the appropriate greeting
 
 var today = new Date();
 var hourNow = today.getHours();
 var greeting;
+
+// The code then sets a conditional statement that checks what time it is and sets a different line of text based on that time and assigns it to greeting
+// If the time cannot be found, it will always display the default
+
 
 if(hourNow > 18) {
   greeting = "Good Evening!";
@@ -21,4 +21,8 @@ if(hourNow > 18) {
   greeting = "Welcome!";
 }
 
+// The script finally calls on the document object to create HTML to display a header tag with the appropriate greeting
+
 document.write('<h3>' + greeting + '<h3>');
+
+// This exercise demonstrates the use of Javascript methods to dynamically update the page
