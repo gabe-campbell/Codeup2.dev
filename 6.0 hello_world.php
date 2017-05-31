d02cfe8592e5fecb05341c46fac6b689635ad1ef
Gabe Campbell <gabe.campbell@icloud.com>
2017-05-31 11:31:11 -0500

    Added 6.0 exercise

---
+++ 6.0 hello_world.php
@@ -0,0 +1,8 @@
+<?php
+
+  $firstName = 'Gabe';
+  $lastName = 'Campbell';
+
+  echo "Hello, my name is " . $firstName . ' ' . $lastName;
+
+  
---
+++ hello_world.php
@@ -0,0 +1,6 @@
+<?php
+
+  $firstName = 'Gabe';
+  $lastName = 'Campbell';
+
+  echo "Hello, my name is " . $firstName . ' ' . $lastName;
